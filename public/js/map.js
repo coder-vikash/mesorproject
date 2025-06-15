7d233b13a8207ece181dd6e097689034d8a9a518
Vikash kumar <vk422821@gmail.com>
2025-06-15 15:59:03 +0530

    Update map.js
    add map pointers

--- public/js/map.js
+++ public/js/map.js
@@ -2,13 +2,14 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  // center: listings.geometry.coordinates,
-  center: [77.209, 28.6139], // starting position [lng, lat]. Note that lat m
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  center: listing.geometry.coordinates,
+  // center: [77.209, 28.6139], // starting position [lng, lat]. Note that lat m
   zoom: 9, // starting zoom
 });
 
 const marker = new mapboxgl.Marker({ color: "red" })
-  // .setLngLat(listing.geometry.coordinates)
+  .setLngLat(listing.geometry.coordinates)
   // .setLngLat(coordinates)
   .setPopup(
     new mapboxgl.Popup({ offset: 25 }).setHTML(
